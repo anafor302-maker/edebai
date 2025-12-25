32cac730261caa5734bdc64221e89770e7584347
anafor302-maker <anafor302@gmail.com>
2025-12-25 21:26:47 +0300

    processors ten mi yedik

--- edebai/settings.py
+++ edebai/settings.py
@@ -53,7 +53,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'blog.context_processors.language_context',
+                #'blog.context_processors.language_context',
                 'blog.context_processors.cookie_consent_context',
             ],
         },
